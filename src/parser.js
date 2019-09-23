d66676c836fcf20832ee7c96afa23a578c62d760
Chris Caragianis <chriscaragianis@gmail.com>
2019-09-23 13:45:02 -0400

    Handle bad digit (#2)
    * set up for parser
    
    * simple case passes, string to acct
    
    * read input from file
    
    * compute checksums
    
    * handle bad character
    
    * write output file

--- src/parser.js
+++ src/parser.js
@@ -1,11 +1,14 @@
 import chunk from 'lodash/chunk';
 import flatten from 'lodash/flatten';
+import keys from 'lodash/keys';
 import map from 'lodash/map';
+import noop from 'lodash/noop';
 import pad from 'lodash/pad';
+import reduce from 'lodash/reduce';
 import zip from 'lodash/zip';
 import fs from 'fs';
 
-const readDigit = {
+const readDigitData = {
   '     |  |': 1,
   ' _  _||_ ': 2,
   ' _  _| _|': 3,
@@ -16,6 +19,13 @@ const readDigit = {
   ' _ |_||_|': 8,
   ' _ |_| _|': 9,
   ' _ | ||_|': 0,
+};
+
+const readDigit = (str) => {
+  if (keys(readDigitData).indexOf(str) === -1) {
+    return '?';
+  }
+  return readDigitData[str];
 };
 
 const lines = (ocrData) => {
@@ -33,20 +43,45 @@ const numberCodes = (chunkLists) => {
   return map(flatCodes, (x) => x.join(''));
 };
 
+const output = (acctNum, chkSm) => {
+  if (chkSm === 0) {
+    return acctNum;
+  }
+  return `${acctNum} ERR`;
+};
+
+export const checkSum = (digitList) => {
+  const pairs = zip([9, 8, 7, 6, 5, 4, 3, 2, 1], digitList);
+  const values = map(pairs, (x) => x[0] * x[1]);
+  return reduce(values, (acc, x) => (acc + x) % 11, 0);
+};
+
 export const parseOCRLine = (ocrLines) => {
   const chunkedLines = map(ocrLines, (x) => chunk(x, 3));
   const codes = numberCodes(chunkedLines);
-  const digits = map(codes, (x) => readDigit[x]);
-  return digits.join('');
+  const digits = map(codes, (x) => readDigit(x));
+  if (digits.indexOf('?') !== -1) {
+    return `${digits.join('')} ILL`;
+  }
+  const acctNum = digits.join('');
+  return output(acctNum, checkSum(digits));
 };
 
-const parseOCRFile = (filepath) => {
-  const fileContents = fs.readFileSync(filepath, 'utf-8');
+const writeFile = (pathOut, data) => {
+  const fileDataOut = `${data.join('\n')}\n`;
+  fs.writeFileSync(pathOut, fileDataOut, noop);
+};
+
+const parseOCRFile = (filepathIn, filepathOut) => {
+  const fileContents = fs.readFileSync(filepathIn, 'utf-8');
   const allLines = lines(fileContents);
   const ocrResultsRaw = chunk(allLines, 4);
   // trim last line of each chunk
   const ocrResults = map(ocrResultsRaw, (x) => x.slice(0, -1));
-  return map(ocrResults, (x) => parseOCRLine(x));
+  const data = map(ocrResults, (x) => parseOCRLine(x));
+
+  writeFile(filepathOut, data);
+  return data;
 };
 
 export default parseOCRFile;
--- src/parser.test.js
+++ src/parser.test.js
@@ -1,7 +1,8 @@
 import assert from 'assert';
+import fs from 'fs';
 import map from 'lodash/map';
 import pad from 'lodash/pad';
-import parseOCRFile, { parseOCRLine } from './parser';
+import parseOCRFile, { checkSum, parseOCRLine } from './parser';
 
 // allow us to write test cases legibly
 const normalizeOCRLine = (ocrLine) => {
@@ -17,16 +18,44 @@ const normalizeOCRLine = (ocrLine) => {
 
 const ocrData = normalizeOCRLine(`
     _  _     _  _  _  _  _
+  | _| _||_||_ |_   ||_||_|
+  ||_  _|  | _||_|  ||_| _|
+`);
+
+const ocrDataErr = normalizeOCRLine(`
+    _  _     _  _  _  _  _
   ||_||_ |_||_ |_   ||_||_|
   ||_| _|  | _||_|  ||_| _|
 `);
 
+const ocrDataIll = normalizeOCRLine(`
+    _  _     _  _  _  _  _
+  | _|  ||_||_ |_   ||_||_|
+  ||_  _|  | _||_|  ||_| _|
+`);
+
+
 // Parse a single line
 export const parseLineTest = () => {
-  assert.equal(parseOCRLine(ocrData), '185456789');
+  assert.equal(parseOCRLine(ocrData), '123456789');
+  assert.equal(parseOCRLine(ocrDataErr), '185456789 ERR');
+  assert.equal(parseOCRLine(ocrDataIll), '12?456789 ILL');
 };
 
 // Read and parse a file
 export const parseFileTest = () => {
-  assert.deepEqual(parseOCRFile('input.txt'), ['185456789', '123756789', '123456789']);
+  assert.deepEqual(parseOCRFile('input.txt', 'new_output.txt'), [
+    '185456789 ERR',
+    '123756789 ERR',
+    '123456789',
+    '1?3456789 ILL',
+  ]);
+  assert.equal(fs.readFileSync('output.txt', 'utf-8'), fs.readFileSync('new_output.txt', 'utf-8'));
+};
+
+// Checksum unit test
+export const checkSumTest = () => {
+  assert.equal(checkSum([1, 1, 1, 1, 1, 1, 1, 1, 1]), 1);
+  assert.equal(checkSum([0, 0, 0, 0, 0, 0, 0, 0, 0]), 0);
+  assert.equal(checkSum([1, 0, 0, 0, 0, 0, 0, 3, 0]), 4);
 };
